1997e8b9671325b818444850975d6c3e2a6f8eee
Daphne Avril <daphnemuseruka@365solutions.io>
2024-12-07 20:09:19 +0200

    Debugging

---
+++ week02/scripts/debugging.js
@@ -0,0 +1,13 @@
+const PI = 3.14;
+let area = 0;
+
+function circleArea(radius) {
+    return radius * PI;
+}
+
+
+area = circleArea(3);
+console.log("Area1:", area);
+
+area = circleArea(4);
+console.log("Area2:", area);
